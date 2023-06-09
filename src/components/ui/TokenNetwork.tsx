c3aaf2b90abd26c1d54349ed450c8c05eb9712ac
Jananadi Wedagedara <jananadiw@gmail.com>
2023-06-09 21:05:26 +0900

    improvement: Utilize component to receive size prop

--- src/components/ui/TokenNetwork.tsx
+++ src/components/ui/TokenNetwork.tsx
@@ -6,17 +6,18 @@ type TokenNetworkSymbolProps = {
   h?: number;
   w2?: number;
   h2?: number;
+  groupWidth?: number;
   tokenType?: string;
   network?: string;
 };
 
 export default function TokenNetworkSymbol(props: TokenNetworkSymbolProps) {
-  const { w, h, network, tokenType } = props;
+  const { w, h, network, tokenType, groupWidth } = props;
 
   return (
     <Flex>
       {tokenType && network && (
-        <Flex position={"relative"}>
+        <Flex position={"relative"} w={groupWidth ? `${groupWidth}px` : "32px"}>
           <TokenSymbol w={w ? w : 32} h={h ? h : 32} tokenType={tokenType} />
           <Flex position={"absolute"} bottom={0} right={0}>
             <NetworkSymbol
