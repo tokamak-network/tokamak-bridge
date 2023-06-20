456c6840541339e9b0d49b16c7a61e7a831eb38c
SonYoungsung <sysgigigi@gmail.com>
2023-06-20 17:21:11 +0900

    Feat : selectedToken removed when it doesn't exist on a changed network

--- src/hooks/modal/useTokenModal.ts
+++ src/hooks/modal/useTokenModal.ts
@@ -1,6 +1,13 @@
-import { tokenModalStatus } from "@/recoil/bridgeSwap/atom";
+import {
+  selectedInTokenStatus,
+  selectedOutTokenStatus,
+  tokenModalStatus,
+} from "@/recoil/bridgeSwap/atom";
 import { searchTokenStatus } from "@/recoil/card/selectCard/searchToken";
+import { useCallback } from "react";
 import { useRecoilState } from "recoil";
+import useConnectedNetwork from "../network";
+import { TokenInfo } from "@/types/token/supportedToken";
 
 export default function useTokenModal() {
   const [tokenModal, setTokenModal] = useRecoilState(tokenModalStatus);
@@ -21,11 +28,41 @@ export default function useTokenModal() {
     setTokenModal({ isOpen: null, modalData: null });
   };
 
+  const [, setSelectedInToken] = useRecoilState(selectedInTokenStatus);
+
+  const [, setSelectedOutToken] = useRecoilState(selectedOutTokenStatus);
+  const { chainName } = useConnectedNetwork();
+
+  const setSelectedToken = useCallback(
+    (
+      tokenData: TokenInfo & {
+        isNew?: boolean | undefined;
+      }
+    ) => {
+      isInTokenOpen && chainName
+        ? setSelectedInToken({
+            ...tokenData,
+            amountBN: null,
+            parsedAmount: null,
+            tokenAddress: tokenData.address[chainName],
+          })
+        : chainName &&
+          setSelectedOutToken({
+            ...tokenData,
+            amountBN: null,
+            parsedAmount: null,
+            tokenAddress: tokenData.address[chainName],
+          });
+    },
+    [chainName]
+  );
+
   return {
     isInTokenOpen,
     isOutTokenOpen,
     onOpenInToken,
     onOpenOutToken,
     onCloseTokenModal,
+    setSelectedToken,
   };
 }
