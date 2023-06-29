e64a0d49c11496ceac4cc7613287f51d33b3fc7b
Lakmi <lakmi94@gmail.com>
2023-06-29 13:43:32 +0900

    Feat : pools - coming soon message

--- src/app/pools/page.tsx
+++ src/app/pools/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import YourPools from "@/pools/YourPools";
-
+import PoolsMessage from "./PoolsMessage";
 export default function Page() {
   return (
     <>
-      <YourPools />
+      <PoolsMessage />
     </>
   );
 }
