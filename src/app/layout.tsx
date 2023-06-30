5f2076203a7d6ac8813fa668fb5fed93a54b12c0
SonYoungsung <sysgigigi@gmail.com>
2023-06-30 18:49:34 +0900

    Feat : Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Center } from "@chakra-ui/react";
 import { RecoilRoot } from "recoil";
 import Header from "@/components/header/Index";
 import HistoryDrawer from "@/components/history/Drawer";
+import Head from "next/head";
 
 import "css/scrollbar.css";
 import { ApolloProvider } from "@apollo/client";
@@ -25,9 +26,29 @@ export const GlobalComponents = () => {
   );
 };
 
-// export const metadata = {
-//   title: "wagmi",
-// };
+const HeadMeta = () => {
+  return (
+    <div>
+      <Head>
+        <title>Tokamak Bridge</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://bridge.tokamak.network" />
+        <meta property="title" content="Tokamak Bridge" />
+        <meta property="og:title" content="Tokamak Bridge" />
+        {/* <meta
+        property="description"
+        content="Functional upgrade to TONStarter ecosystem"
+      />
+      <meta
+        property="og:description"
+        content="Functional upgrade to TONStarter ecosystem"
+      /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
 
 export default function RootLayout({
   children,
@@ -37,12 +58,9 @@ export default function RootLayout({
   const queryClient = getQueryClient();
   return (
     <html lang="en">
-      <body
-        style={{ maxHeight: "100vh", margin: 0, padding: 0 }}
-        //prevent extensions from causing a mismatch
-        // suppressHydrationWarning={true}
-      >
+      <body style={{ maxHeight: "100vh", margin: 0, padding: 0 }}>
         <RecoilRoot>
+          <HeadMeta />
           <QueryClientProvider client={queryClient}>
             <ApolloProvider client={apolloClient}>
               <ChakraProvidersForNextJs>
