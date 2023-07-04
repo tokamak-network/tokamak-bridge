ba997e4f263e6575326ac68778288b76b0fa685c
cd4761 <cd4761@gmail.com>
2023-07-04 13:52:29 +0900

    fix for graphql

--- src/graphql/pools/queries.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const GET_POSITIONS = gql`
-  query Positions($account: String!) @api(contextKey: "apiName") {
-    positions(owner: $account) {
-      id
-    }
-  }
-`;
