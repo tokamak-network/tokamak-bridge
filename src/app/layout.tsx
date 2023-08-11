1437d0ed1b5533b58220e04c4a604e6c70cc1d58
SonYoungsung <sysgigigi@gmail.com>
2023-08-11 11:13:19 +0900

    Feat : Head - update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { ChakraProvidersForNextJs } from "@/providers/chakraProvider";
 import { WagmiProviders } from "@/providers/wagmiProvider";
 import { RecoilRoot } from "recoil";
 import HistoryDrawer from "@/components/history/Drawer";
-import Head from "next/head";
 
 import "css/scrollbar.css";
 import Modals from "./Modals";
@@ -12,8 +11,6 @@ import TxToast from "@/components/modal/TxToast";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { getQueryClient } from "@/client/queryClient";
 import Entry from "./Entry";
-import { url } from "inspector";
-import { Metadata } from "next";
 
 export const GlobalComponents = () => {
   return (
@@ -24,26 +21,17 @@ export const GlobalComponents = () => {
   );
 };
 
-const metadata: Metadata = {
-  title: "Tokamak Bridge",
-  description: "https://bridge.tokamak.network",
-  viewport: "width=device-width, initial-scale=1.0",
-  publisher: "Vercel",
-};
-
 export const HeadMeta = () => {
   return (
-    <div>
-      <Head>
-        <title>Tokamak Bridge</title>
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://bridge.tokamak.network" />
-        <meta property="title" content="Tokamak Bridge" />
-        <meta property="og:title" content="Tokamak Bridge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
+    <head>
+      <title>Tokamak Bridge</title>
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://bridge.tokamak.network" />
+      <meta property="title" content="Tokamak Bridge" />
+      <meta property="og:title" content="Tokamak Bridge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href="/favicon.ico" />
+    </head>
   );
 };
 
