0cb7e03a804fadf81bd8cd459a2c2d33e319346b
SonYoungsung <sysgigigi@gmail.com>
2023-08-14 15:31:12 +0900

    Fix : YourPool - change height to the condition

--- src/app/pools/YourPools.tsx
+++ src/app/pools/YourPools.tsx
@@ -1,9 +1,12 @@
 import { Flex, Text } from "@chakra-ui/react";
 import PoolList from "./components/PoolList";
 import { useAccount } from "wagmi";
+import { useGetPositions } from "@/hooks/pool/useGetPositionIds";
 
 export default function YourPools() {
   const { isConnected } = useAccount();
+  const { positions } = useGetPositions();
+  const isReducedHeight = isConnected || positions?.length === 0;
   return (
     <Flex flexDir="column" alignSelf={"baseline"} mt={"180px"}>
       <Text fontSize={36} fontWeight="medium" marginBottom="24px">
@@ -11,7 +14,7 @@ export default function YourPools() {
       </Text>
       <Flex
         w="672px"
-        h={isConnected ? "600px" : "400px"}
+        h={isReducedHeight ? "440px" : "600px"}
         alignItems="flex-start"
         p={"20px"}
         pr={"0px"}
