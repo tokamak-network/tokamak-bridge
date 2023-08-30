db716dc17857c8dba1bee84e542814adfdfeb350
SonYoungsung <sysgigigi@gmail.com>
2023-08-30 14:52:40 +0900

    Feat : Info - remove action icons when it's not an owner

--- src/app/pools/[info]/components/Liquidity.tsx
+++ src/app/pools/[info]/components/Liquidity.tsx
@@ -13,6 +13,7 @@ import { useRouter } from "next/navigation";
 import { useCallback } from "react";
 import { smallNumberFormmater } from "@/utils/number/compareNumbers";
 import { splitNumber } from "@/utils/trim/splitNumber";
+import { useAccount } from "wagmi";
 
 const TokenLiquidityData = (props: {
   token: Token;
@@ -62,7 +63,17 @@ export default function Liquidity() {
   }
 
   const { ratio } = usePoolInfo();
-  const { token0, token0Amount, token1, token1Amount, isClosed } = info;
+  const {
+    token0,
+    token0Amount,
+    token1,
+    token1Amount,
+    isClosed,
+    token0MarketPrice,
+    token1MarketPrice,
+    owner,
+  } = info;
+  const { address } = useAccount();
 
   const { totalMarketPrice } = usePricePair({
     token0Name: token0.name,
@@ -72,12 +83,17 @@ export default function Liquidity() {
   });
 
   const router = useRouter();
+  const noMarketPrices =
+    (token0MarketPrice === undefined && token1MarketPrice === undefined) ||
+    Number(token0MarketPrice) + Number(token1MarketPrice) === 0;
 
   const onClickToRoute = useCallback(async (remove?: boolean) => {
     return router.push(
       remove ? `/pools/remove/${info.id}` : `/pools/increase/${info.id}/`
     );
   }, []);
+
+  const actionDisabled = owner !== address;
 
   return (
     <Box
@@ -89,36 +105,38 @@ export default function Liquidity() {
       alignItems="center"
     >
       <Flex flexDir="column" alignItems={"center"}>
-        <Flex w={"100%"} justifyContent={"space-between"} px={"10px"}>
-          <Flex
-            flexDir={"column"}
-            alignItems={"center"}
-            rowGap={"20px"}
-            pt={"2px"}
-          >
-            <Text color="#A0A3AD" fontSize={13}>
-              Remove
-            </Text>
-            <Box
-              onClick={() => (isClosed ? null : onClickToRoute(true))}
-              cursor={isClosed ? "" : "pointer"}
+        <Flex w={"100%"} justifyContent={"center"} px={"10px"}>
+          {!actionDisabled && (
+            <Flex
+              flexDir={"column"}
+              alignItems={"center"}
+              rowGap={"20px"}
+              pt={"2px"}
             >
-              <Flex
-                w={"32px"}
-                h={"32px"}
-                bg="#15161D"
-                border={isClosed ? "" : " 1px solid #007AFF"}
-                borderRadius={"8px"}
-                alignItems={"center"}
-                justifyContent={"center"}
+              <Text color="#A0A3AD" fontSize={13}>
+                Remove
+              </Text>
+              <Box
+                onClick={() => (isClosed ? null : onClickToRoute(true))}
+                cursor={isClosed ? "" : "pointer"}
               >
-                <Image
-                  src={isClosed ? RemoveInactiveIcon : RemoveIcon}
-                  alt={"RemoveIcon"}
-                />
-              </Flex>
-            </Box>
-          </Flex>
+                <Flex
+                  w={"32px"}
+                  h={"32px"}
+                  bg="#15161D"
+                  border={isClosed ? "" : " 1px solid #007AFF"}
+                  borderRadius={"8px"}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                >
+                  <Image
+                    src={isClosed ? RemoveInactiveIcon : RemoveIcon}
+                    alt={"RemoveIcon"}
+                  />
+                </Flex>
+              </Box>
+            </Flex>
+          )}
           <Flex
             flexDir={"column"}
             alignItems={"center"}
@@ -128,33 +146,39 @@ export default function Liquidity() {
             <Text fontSize={"16px"} h={"24px"}>
               Liquidity
             </Text>
-            <Text fontSize={"38px"} height={"57px"}>
-              {`$${splitNumber(totalMarketPrice)}`}
+            <Text
+              fontSize={"38px"}
+              height={"57px"}
+              color={noMarketPrices ? "#A0A3AD" : ""}
+            >
+              {noMarketPrices ? "-" : `$${splitNumber(totalMarketPrice)}`}
             </Text>
           </Flex>
-          <Flex
-            flexDir={"column"}
-            alignItems={"center"}
-            rowGap={"20px"}
-            pt={"2px"}
-          >
-            <Text color="#A0A3AD" fontSize={13}>
-              Increase
-            </Text>
-            <Box onClick={() => onClickToRoute(false)} cursor={"pointer"}>
-              <Flex
-                w={"32px"}
-                h={"32px"}
-                bg="#15161D"
-                border={" 1px solid #007AFF"}
-                borderRadius={"8px"}
-                alignItems={"center"}
-                justifyContent={"center"}
-              >
-                <Image src={IncreaseIcon} alt={"IncreaseLiquidity"} />
-              </Flex>
-            </Box>
-          </Flex>
+          {!actionDisabled && (
+            <Flex
+              flexDir={"column"}
+              alignItems={"center"}
+              rowGap={"20px"}
+              pt={"2px"}
+            >
+              <Text color="#A0A3AD" fontSize={13}>
+                Increase
+              </Text>
+              <Box onClick={() => onClickToRoute(false)} cursor={"pointer"}>
+                <Flex
+                  w={"32px"}
+                  h={"32px"}
+                  bg="#15161D"
+                  border={" 1px solid #007AFF"}
+                  borderRadius={"8px"}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                >
+                  <Image src={IncreaseIcon} alt={"IncreaseLiquidity"} />
+                </Flex>
+              </Box>
+            </Flex>
+          )}
         </Flex>
         <Box w={"100%"} h={"1px"} bgColor={"#313442"} />
         <Flex
