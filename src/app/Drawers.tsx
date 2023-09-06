e5b5839d6939bb5ae1a82be83dfa566b7eb01053
Lakmi <lakmi94@gmail.com>
2023-09-06 17:41:46 +0900

    Update Drawers.tsx

--- src/app/Drawers.tsx
+++ src/app/Drawers.tsx
@@ -1,5 +1,4 @@
 import AccountHistory from "@/components/history/AccountHistory";
-import useGetTransaction from "@/hooks/user/useGetTransaction";
 import { useEffect, useMemo, useState } from "react";
 import { useAccount } from "wagmi";
 import { tData } from "@/types/activity/history";
