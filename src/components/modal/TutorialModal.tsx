4aea22567d2459f70aca9907546b95aded77ec76
Lakmi <lakmi94@gmail.com>
2023-09-19 16:10:43 +0900

    Feat: make the tutorial modal Draggable

--- src/components/modal/TutorialModal.tsx
+++ src/components/modal/TutorialModal.tsx
@@ -11,7 +11,7 @@ import { useDisclosure } from "@chakra-ui/react";
 import Image from "next/image";
 import LOGO_IMAGE from "assets/icons/serviceLogo.svg";
 import CloseButton from "../button/CloseButton";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import step0 from "assets/image/tutorial/step0.svg";
 import step1 from "assets/image/tutorial/step1.svg";
 import step2 from "assets/image/tutorial/step2.svg";
@@ -19,6 +19,7 @@ import step3 from "assets/image/tutorial/step3.svg";
 import step4 from "assets/image/tutorial/step4.svg";
 import step5 from "assets/image/tutorial/step5.svg";
 import { useLocalStorage } from "@/hooks/storage/useLocalStorage";
+import { motion } from "framer-motion";
 
 const steps = [
   {
@@ -72,18 +73,57 @@ const bgs = [
   { bg: step0, px: "15px", size: "496px 463px", mt: "127px !important" },
 ];
 
+type Size = {
+  top: number ;
+  left: number;
+  bottom: number;
+  right:number
+};
+
 export default function TutorialModal() {
   const [currentStep, setCurrentStep] = useState<number>(0);
   const [, setModalOpen] = useState(true);
-
+  const [size, setSize] = useState<Size>({
+    top: 0,
+  left: 0,
+  bottom: 0,
+  right:0,
+  });
   const [storedValue, setValue] = useLocalStorage("tutorial", false);
   const closeModal = useCallback(() => {
     setValue(true);
     setModalOpen(false);
   }, []);
+  const constraintsRef = useRef<HTMLDivElement | null>(null);
 
   const isOpen = storedValue === false;
 
+  useEffect(() => {
+    const getHeightWidth = () => {
+      if (constraintsRef) {
+
+        const height = constraintsRef?.current?.offsetHeight
+        const width = constraintsRef?.current?.offsetWidth        
+        setSize({
+          top: (Number(height)/-2)+180,
+          left: (Number(width)/-2)+202,
+          right: Number(width)/2-202,
+          bottom: Number(height)/2-180,
+        });
+      }
+    };
+
+    getHeightWidth();
+    const handleResize = () => {
+      getHeightWidth();
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [constraintsRef?.current, setSize]);
+  
   return (
     <Modal onClose={() => {}} isOpen={isOpen} isCentered>
       <ModalOverlay
@@ -97,18 +137,25 @@ export default function TutorialModal() {
           backgroundPositionY: ["center"],
           backgroundPositionX: "center",
         }}
+        ref={constraintsRef}
       />
+
       <ModalContent
+        as={motion.div}
+        drag
+        dragElastic={{top: 0, bottom: 0.3}}
+        // dragTransition={{ bounceStiffness: 100, bounceDamping: 1000 }}
+        dragConstraints={size}
+        dragMomentum={false}
         justifyContent={"center"}
         alignItems={"center"}
-        m={0}
+      
         borderRadius={"24px"}
         w="404px"
         paddingBottom={"40px"}
         bg={"#1F2128"}
-        mt={bgs[currentStep].mt}
-        position={"fixed"}
-      >
+      
+        position={"fixed"}>
         <Flex flexDir={"column"} alignItems={"center"}>
           <Flex w={"100%"} justifyContent={"flex-end"} pt={"14px"} pr={"14px"}>
             <CloseButton onClick={closeModal} />
@@ -127,38 +174,43 @@ export default function TutorialModal() {
             {steps[currentStep].stepTitle}
           </Text>
           <Flex ml={"39px"} mr={"41px"} flexDir={"column"}>
-            {steps[currentStep].stepDescription.map((text: string) => {
-              return currentStep !== 5 ? (
-                <Text
-                key={text}
-                  textAlign={currentStep === 0 ? "center" : "left"}
-                  mb={"40px"}
-                >
-                  {text}
-                </Text>
-              ) : (
-                <Text mb={"16px"} textAlign={"left"}   key={text}>
-                  To get a more in-depth guide on how to use Tokamak Bridge, you
-                  can find a link to our{" "}
-                  <Link
-                    style={{ color: "#007AFF", textDecoration: "underline" }}
-                    href="https://tokamaknetwork.gitbook.io/home/02-service-guide/tokamak-bridge"
-                    isExternal
-                  >
-                    user guide
-                  </Link>{" "}
-                  under the “More” tab in the menu. You can also request help
-                  from our team by filling out this google{" "}
-                  <Link
-                    style={{ color: "#007AFF", textDecoration: "underline" }}
-                    href="https://docs.google.com/forms/u/1/d/e/1FAIpQLSfCUJjuABK0Locc3Fqwr2W5eHI-Hpj6wiiGceBr1e4q4g9nmg/viewform?usp=send_form"
-                    isExternal
-                  >
-                    form
-                  </Link>
-                </Text>
-              );
-            })}
+            {steps[currentStep].stepDescription.map(
+              (text: string, index: number) => {
+                return currentStep !== 5 ? (
+                  <Text
+                    key={index}
+                    textAlign={currentStep === 0 ? "center" : "left"}
+                    mb={"40px"}>
+                    {text}
+                  </Text>
+                ) : (
+                  <Text mb={"16px"} textAlign={"left"} key={text}>
+                    To get a more in-depth guide on how to use Tokamak Bridge,
+                    you can find a link to our{" "}
+                    <Link
+                      style={{
+                        color: "#007AFF",
+                        textDecoration: "underline",
+                      }}
+                      href="https://tokamaknetwork.gitbook.io/home/02-service-guide/tokamak-bridge"
+                      isExternal>
+                      user guide
+                    </Link>{" "}
+                    under the “More” tab in the menu. You can also request help
+                    from our team by filling out this google{" "}
+                    <Link
+                      style={{
+                        color: "#007AFF",
+                        textDecoration: "underline",
+                      }}
+                      href="https://docs.google.com/forms/u/1/d/e/1FAIpQLSfCUJjuABK0Locc3Fqwr2W5eHI-Hpj6wiiGceBr1e4q4g9nmg/viewform?usp=send_form"
+                      isExternal>
+                      form
+                    </Link>
+                  </Text>
+                );
+              }
+            )}
           </Flex>
 
           <Flex
@@ -166,15 +218,13 @@ export default function TutorialModal() {
             pr={"41px"}
             w="100%"
             justifyContent={"center"}
-            alignItems={"center"}
-          >
+            alignItems={"center"}>
             {currentStep !== 0 ? (
               <Text
                 color={"#FFF"}
                 opacity={0.5}
                 cursor={"pointer"}
-                onClick={() => setCurrentStep(currentStep - 1)}
-              >
+                onClick={() => setCurrentStep(currentStep - 1)}>
                 Back
               </Text>
             ) : (
@@ -183,12 +233,11 @@ export default function TutorialModal() {
             <Flex
               flexDir={"row"}
               ml={"87px"}
-              mr={currentStep === 5 ? "60px" : "95px"}
-            >
+              mr={currentStep === 5 ? "60px" : "95px"}>
               {steps.map((step: any, index: number) => {
                 return (
                   <Flex
-                  key={step}
+                    key={step.stepTitle}
                     h="5px"
                     w={"5px"}
                     mr={index === 5 ? "0px" : "7px"}
@@ -203,8 +252,7 @@ export default function TutorialModal() {
             {currentStep !== 5 ? (
               <Text
                 cursor={"pointer"}
-                onClick={() => setCurrentStep(currentStep + 1)}
-              >
+                onClick={() => setCurrentStep(currentStep + 1)}>
                 Next
               </Text>
             ) : (
