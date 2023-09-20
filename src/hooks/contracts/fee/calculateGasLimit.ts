2539c1b84f1a69a56773034242d426f8ea19057e
SonYoungsung <sysgigigi@gmail.com>
2023-09-20 14:37:15 +0900

    Feat : Pools - disable calculating gasFee on L2 until gasFee is changed on Titan

--- src/hooks/contracts/fee/calculateGasLimit.ts
+++ src/hooks/contracts/fee/calculateGasLimit.ts
@@ -16,15 +16,18 @@ export async function calculateGasLimit(
     return calculateGasMargin(estimatedGas);
   }
 
-  const gasPrice = await provider.getGasPrice();
-  const l2ProSDK = titanSDK.asL2Provider(
-    providerByChainId[
-      isConnectedToMainNetwork
-        ? SupportedChainId.TITAN
-        : SupportedChainId.DARIUS
-    ]
-  );
-  const totalGasCost = await l2ProSDK.estimateTotalGasCost(tx);
-  const estimatedGas = BigNumber.from(totalGasCost).div(gasPrice);
+  const estimatedGas = await provider.estimateGas(tx);
   return calculateGasMargin(estimatedGas);
+
+  // const gasPrice = await provider.getGasPrice();
+  // const l2ProSDK = titanSDK.asL2Provider(
+  //   providerByChainId[
+  //     isConnectedToMainNetwork
+  //       ? SupportedChainId.TITAN
+  //       : SupportedChainId.DARIUS
+  //   ]
+  // );
+  // const totalGasCost = await l2ProSDK.estimateTotalGasCost(tx);
+  // const estimatedGas = BigNumber.from(totalGasCost).div(gasPrice);
+  // return calculateGasMargin(estimatedGas);
 }
