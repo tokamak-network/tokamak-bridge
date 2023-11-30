9536e8bb6bee47c74f994ed9222a551ed9de31ad
Lakmi <lakmi94@gmail.com>
2023-11-30 17:59:36 +0900

    temp

--- src/app/BridgeSwap/Swap.tsx
+++ src/app/BridgeSwap/Swap.tsx
@@ -6,16 +6,25 @@ import OutToken from "./components/OutToken";
 import Image from "next/image";
 import ArrowImg from "assets/icons/arrow.svg";
 import SelectNetwork from "./components/SelectNetwork";
-import { actionMode } from "@/recoil/bridgeSwap/atom";
-import { useRecoilValue } from "recoil";
+import { actionMode, selectedInTokenStatus, selectedOutTokenStatus } from "@/recoil/bridgeSwap/atom";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 export default function Swap() {
   const { mode } = useRecoilValue(actionMode);
+  const [inTokenRecoilValue, setInTokenRecoilValue] = useRecoilState(
+    selectedInTokenStatus
+  );
+  const [outTokenRecoilValue, setOutTokenRecoilValue] = useRecoilState(
+    selectedOutTokenStatus
+  );
 
+  console.log(inTokenRecoilValue,outTokenRecoilValue);
+  
   return (
     <Flex w={"100%"} justifyContent={"space-between"} columnGap={"14.6px"}>
       <InToken />
       <Flex
+      // onClick={}
         justifyContent={"center"}
         alignItems={"center"}
         pt={mode === null ? "65px" : "80px"}
