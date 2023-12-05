b5a2f1c6709b55f8eef70343acd5e7214e84ec1a
Lakmi <lakmi94@gmail.com>
2023-12-05 16:53:04 +0900

    temp

--- src/app/BridgeSwap/Swap.tsx
+++ src/app/BridgeSwap/Swap.tsx
@@ -4,7 +4,11 @@ import Image from "next/image";
 import { Flex, Text, Box } from "@chakra-ui/layout";
 import { useRecoilValue, useRecoilState } from "recoil";
 
-import { actionMode } from "@/recoil/bridgeSwap/atom";
+import {
+  actionMode,
+  selectedInTokenStatus,
+  selectedOutTokenStatus,
+} from "@/recoil/bridgeSwap/atom";
 import { actionMethodStatus, swapSettingStatus } from "@/recoil/modal/atom";
 import useMediaView from "@/hooks/mediaView/useMediaView";
 
@@ -13,10 +17,10 @@ import OutToken from "./components/OutToken";
 import SelectNetwork from "./components/SelectNetwork";
 import MobileInToken from "./components/Mobile/MobileInToken";
 import MobileOutToken from "./components/Mobile/MobileOutToken";
-
 import ArrowImg from "assets/icons/arrow.svg";
 import arrow from "assets/icons/dark_arrowdown.svg";
 import SettingIcon from "assets/icons/setting.svg";
+import { useCallback } from "react";
 
 export default function Swap() {
   const { mode } = useRecoilValue(actionMode);
@@ -25,17 +29,37 @@ export default function Swap() {
 
   const { pcView, mobileView } = useMediaView();
 
+  const [inTokenRecoilValue, setInTokenRecoilValue] = useRecoilState(
+    selectedInTokenStatus
+  );
+  const [outTokenRecoilValue, setOutTokenRecoilValue] = useRecoilState(
+    selectedOutTokenStatus
+  );
+  const invertTokenPair = useCallback(() => {
+    if (inTokenRecoilValue && outTokenRecoilValue) {
+      setInTokenRecoilValue(outTokenRecoilValue);
+      return setOutTokenRecoilValue(inTokenRecoilValue);
+    }
+  }, [inTokenRecoilValue, outTokenRecoilValue]);
+
+  console.log('inTokenRecoilValue',inTokenRecoilValue,'outTokenRecoilValue',outTokenRecoilValue );
+  
   return (
     <>
       {pcView ? (
         <Flex w={"100%"} justifyContent={"space-between"} columnGap={"14.6px"}>
           <InToken />
           <Flex
+            onClick={invertTokenPair}
+            cursor={
+              mode === "Swap" || mode === "Unwrap" || mode === "Wrap"
+                ? "pointer"
+                : ""
+            }
             justifyContent={"center"}
             alignItems={"center"}
-            pt={mode === null ? "65px" : "80px"}
-          >
-            <Image src={ArrowImg} alt={"arrow"} />
+            pt={mode === null ? "65px" : "80px"}>
+            <Image src={ArrowImg} alt={"arrow"} width={24} />
           </Flex>
           {mode === null ? <SelectNetwork /> : <OutToken />}
         </Flex>
@@ -45,8 +69,7 @@ export default function Swap() {
             <Flex
               columnGap={"8px"}
               cursor={"pointer"}
-              onClick={() => setMethodStatus(true)}
-            >
+              onClick={() => setMethodStatus(true)}>
               <Text fontWeight={500} fontSize={24}>
                 {mode === null ? "Swap" : mode.replaceAll("ETH-", "")}
               </Text>
@@ -66,7 +89,7 @@ export default function Swap() {
           <Flex w={"100%"} mx={"auto"} columnGap={"8px"} justify={"center"}>
             <MobileInToken />
 
-            <Flex justifyContent={"center"} alignItems={"center"}>
+            <Flex justifyContent={"center"} alignItems={"center"}  onClick={invertTokenPair}>
               <Image src={ArrowImg} alt={"arrow"} />
             </Flex>
 
