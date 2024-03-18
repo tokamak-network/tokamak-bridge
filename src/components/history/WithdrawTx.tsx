883e0843a10f36f7f7e4ffd05efca801ddd3cb40
jusdy <pauleta0126@gmail.com>
2024-03-18 14:05:59 +0900

    test: deploy

--- src/components/history/WithdrawTx.tsx
+++ src/components/history/WithdrawTx.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Flex, Text, Button, Link } from "@chakra-ui/react";
+import { Flex, Text, Button } from "@chakra-ui/react";
 import TokenPairTx from "./TokenPairTx";
 import StatusTx from "./StatusTx";
 import { ethers } from "ethers";
