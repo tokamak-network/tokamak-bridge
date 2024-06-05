5005cc308c93a7d0298130dd1cd1f7dda2942137
tyrannojung <tyranno2020@gmail.com>
2024-06-05 13:30:09 +0900

    Fix: update image box size

--- src/components/fw/coming/pool/ImagePoolBox.tsx
+++ src/components/fw/coming/pool/ImagePoolBox.tsx
@@ -19,7 +19,7 @@ export default function ImagePoolBox(props: ImageBoxProp) {
       border={"1px solid #313442"}
       borderRadius={"16px"}
       width={isMobile ? undefined : "536px"}
-      height={isMobile ? "72%" : "549px"}
+      height={isMobile ? "85vh" : "549px"}
     >
       <Box zIndex={1}>
         <Image
@@ -28,7 +28,7 @@ export default function ImagePoolBox(props: ImageBoxProp) {
           style={{
             width: isMobile ? undefined : "536px",
             height: isMobile
-              ? "72%"
+              ? "85vh"
               : "549px" /** %단위로 잡아서 진행해 본다. */,
             objectPosition: "top",
             objectFit: "cover",
