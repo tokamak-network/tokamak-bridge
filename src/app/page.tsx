56093a0b6606e8c909a97dc7ad842b1091a77bf0
SonYoungsung <sysgigigi@gmail.com>
2024-08-13 20:27:35 +0900

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { Flex } from "@chakra-ui/react";
 import BridgeSwap from "./BridgeSwap/Index";
 
+//ci/cd test
 export default function Page() {
   return (
     <Flex alignItems={"center"} h={"100%"}>
