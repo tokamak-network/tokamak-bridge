84e0005a79cf8252fa57b42832b016c3741a3ab9
vihsonic <black02430horse@gmail.com>
2024-10-21 10:08:43 +0200

    fix withdraw transaction history shown up due to dev update

--- src/utils/history/getTransaction.ts
+++ src/utils/history/getTransaction.ts
@@ -14,9 +14,10 @@ import { TITAN_CHALLENGE_PERIOD } from "@/constant/network/titan";
 import { SentMessages } from "@/types/activity/history";
 import { SupportedChainId } from "@/types/network/supportedNetwork";
 import { OVM_ETH_BRIDGE } from "@/constant/contracts";
+import { isThanosChain } from "../network/checkNetwork";
 
 const getTokenInfo = (tokenAddress: string, chainId: number) => {
-  if (tokenAddress === OVM_ETH_BRIDGE || tokenAddress === "") {
+  if ((tokenAddress === OVM_ETH_BRIDGE || tokenAddress === "") && !isThanosChain(chainId)) {
     return {
       name: "ETH",
       symbol: "ETH",
