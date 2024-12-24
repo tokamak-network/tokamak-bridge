02570d48e8f8c382f5b249313b7200ed731b2b94
vihsonic <black02430horse@gmail.com>
2024-12-24 06:45:18 +0100

    rollback bridge version to 0.1

--- src/staging/constants/legacyTitan.ts
+++ src/staging/constants/legacyTitan.ts
@@ -4,9 +4,9 @@ import {
 } from "../types/legacyTitan";
 
 export const BRIDGE_VERSION: LegacyTitanBridgeVersionEnum =
-  LegacyTitanBridgeVersionEnum.V10;
+  LegacyTitanBridgeVersionEnum.V01;
 export const LegacyTitanMaintenanceStatus: LegacyTitanMaintenanceEnum =
-  LegacyTitanMaintenanceEnum.DONE;
+  LegacyTitanMaintenanceEnum.IN_PROGRESS;
 export const ClaimFeatureOpenDate = new Date("2025-01-06");
 export const BridgeShutdownDate = new Date("2025-01-13");
 export const TitanShutdownDate = new Date("2024-12-26");
